f8264c0a606f5a7a055d0243d2b2025118287f8c
commenthol <commenthol@gmail.com>
2014-11-22 16:16:56 +0100

    brand-model

---
+++ Makefile
@@ -0,0 +1,24 @@
+PWD = $(shell pwd)
+
+all: prep test 
+
+prep:
+	#git submodule update --init
+	#sudo apt-get install python-yaml
+	
+test:
+	@#test ! -d tmp && mkdir tmp
+	@export PYTHONPATH=tmp && python setup.py develop -d tmp
+	@# run all tests
+	@python ua_parser/user_agent_parser_test.py
+	@# run a single test
+	@#python ua_parser/user_agent_parser_test.py ParseTest.testStringsDeviceBrandModel
+
+clean:
+	@rm ua_parser/user_agent_parser.pyc\
+	   ua_parser/regexes.yaml\
+	   ua_parser/regexes.json
+	@rm -rf tmp\
+	   ua_parser.egg-info
+
+.PHONY: all clean
---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+from setuptools import setup
+from setuptools.command.develop import develop as _develop
+from setuptools.command.sdist import sdist as _sdist
+
+
+def install_regexes():
+    print('Copying regexes.yaml to package directory...')
+    import os
+    import shutil
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    yaml_src = os.path.join(cwd, 'uap-core', 'regexes.yaml')
+    if not os.path.exists(yaml_src):
+        raise RuntimeError(
+                  'Unable to find regexes.yaml, should be at %r' % yaml_src)
+    yaml_dest = os.path.join(cwd, 'ua_parser', 'regexes.yaml')
+    shutil.copy2(yaml_src, yaml_dest)
+
+    print('Converting regexes.yaml to regexes.json...')
+    import json
+    import yaml
+    json_dest = yaml_dest.replace('.yaml', '.json')
+    regexes = yaml.load(open(yaml_dest))
+    with open(json_dest, "w") as f:
+        json.dump(regexes, f)
+
+
+class develop(_develop):
+    def run(self):
+        install_regexes()
+        _develop.run(self)
+
+
+class sdist(_sdist):
+    def run(self):
+        install_regexes()
+        _sdist.run(self)
+
+
+setup(
+    name='ua-parser',
+    version='0.4.0',
+    description="Python port of Browserscope's user agent parser",
+    author='PBS',
+    author_email='no-reply@pbs.org',
+    packages=['ua_parser'],
+    package_dir={'': '.'},
+    license='LICENSE.txt',
+    zip_safe=False,
+    url='https://github.com/ua-parser/uap-python',
+    include_package_data=True,
+    package_data={'ua_parser': ['regexes.yaml', 'regexes.json']},
+    install_requires=['pyyaml'],
+    cmdclass={
+        'develop': develop,
+        'sdist':   sdist,
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
