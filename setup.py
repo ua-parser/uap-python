649ca139bf031806b0e312da6a1ea69e5e0f1aaf
Matt Robenolt <matt@ydekproductions.com>
2015-12-21 18:33:37 -0800

    0.6.1

--- setup.py
+++ setup.py
@@ -174,7 +174,7 @@ cmdclass = {
 
 setup(
     name='ua-parser',
-    version='0.6.0',
+    version='0.6.1',
     description="Python port of Browserscope's user agent parser",
     author='PBS',
     author_email='no-reply@pbs.org',
