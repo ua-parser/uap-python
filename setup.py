9fd85b418e9f76306076012e798bdd652041686f
masklinn <github.com@masklinn.net>
2022-08-20 20:42:15 +0200

    Release 0.15.1
    - removed version bound from `setup_requires`, allows building from
      source when only PyYaml 6.0 is available (and downloading 5.4 is
      not), #121, #125
    - added type assertion to parser lookup, to make the error clearer on
      type error, #122, #123
    - fixed `setup.py build_regexes` and `setup.py build` for non-in-place
      builds, #127

--- setup.py
+++ setup.py
@@ -200,7 +200,7 @@ cmdclass = {
 
 setup(
     name="ua-parser",
-    version="0.15.0",
+    version="0.15.1",
     description="Python port of Browserscope's user agent parser",
     author="PBS",
     author_email="no-reply@pbs.org",
